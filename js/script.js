4df782a88073ca79ae1333084d2b428117d5e465
koladeg <tayokay@yahoo.com>
2020-06-23 20:13:19 +0100

    Add_fish_and_fishherman

--- js/script.js
+++ js/script.js
@@ -14,18 +14,39 @@ const ctx = canvas.getContext('2d');
 let lake = new Image()
 lake.src ='images/background.png'
 let fisherman = new Image()
-fisherman.src = '/Users/koladegureje/Desktop/test-vs-code/IRONHACK/Ironhack1/IronhackProj1/Proj1/images/fishman.png'
+let fishermanXaxis = -40
+let fishermanEndPosition = (canvas.width/2) 
+fisherman.src = 'images/fishman.png'
 let animationId
-console.log(animationId)
-
-// ********************* Functions *********************************
+let fish = []
+let hook = new Image()
+hook.src = 'images/hook.jpg'
+// ********************* Functions *******************************
 function animation() {
-    let animationId = window.requestAnimationFrame(animation)
-
+    animationId = window.requestAnimationFrame(animation)
+    ctx.clearRect(0, 0, canvas.width, canvas.height)
+    if( fishermanXaxis >= fishermanEndPosition){
+        fishermanXaxis = fishermanEndPosition 
+    }
     ctx.drawImage(lake, 0, 0, canvas.height, canvas.width)
-    ctx.drawImage(fisherman, 200, 200, 200, 150)
-    
+    ctx.drawImage(fisherman, fishermanXaxis++, 120, 200, 150) 
+    fish.forEach(fh => {
+        ctx.fillRect(fh.x +=Math.random() * (5 - 2) + 2 , fh.y, fh.width, fh.height )
+    });
+    ctx.drawImage(hook, fishermanXaxis-1, 125, 10, 15)
 }
+function makeFish() {
+    let newFish =  {
+        x:-10,
+        y: Math.random() * (750 - 350) + 350, 
+        width: 20, 
+        height: 8,
+     } 
+     fish.push(newFish)   
+}
+setInterval( makeFish, Math.random() * (3000 - 100) + 100);
+
+ 
 
 // ************************  Script **********************************
 
